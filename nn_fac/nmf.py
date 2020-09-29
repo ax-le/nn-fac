Fixing a bug in the error calculation: badly handled square power between reconstruction errors, normalization and sparsity parameters. + Changing "errors" to "costs" in order to correctly name the content of the variable (may be confusing otherwise).

nn_fac/nmf.py
@@ -13,8 +13,7 @@ from nimfa.methods import seeding
 
 def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol=1e-8,
            sparsity_coefficients = [None, None], fixed_modes = [], normalize = [False, False],
-           verbose=False, return_errors=False):
-
+           verbose=False, return_costs=False):
     """
     ======================================
     Nonnegative Matrix Factorization (NMF)
@@ -22,7 +21,7 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
 
     Factorization of a matrix M in two nonnegative matrices U and V,
     such that the product UV approximates M.
-    If M is of size m*n, U and V are resepctively of size m*r and r*n,
+    If M is of size m*n, U and V are respectively of size m*r and r*n,
     r being the rank of the decomposition (parameter)
     Typically, this method is used as a dimensionality reduction technique,
     or for source separation.
@@ -74,9 +73,9 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two succesive iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficients: List of float (two)
         The sparsity coefficients on U and V respectively.
@@ -86,32 +85,32 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
         Has to be set not to update a factor, 0 and 1 for U and V respectively
         Default: []
     normalize: List of boolean (two)
-        A boolean whereas the factors need to be normalized.
+        Indicates whether the factors need to be normalized or not.
         The normalization is a l_2 normalization on each of the rank components
         (columnwise for U, linewise for V)
         Default: [False, False]
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
     -------
     U, V: numpy arrays
         Factors of the NMF
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list
-        A list with accumulated time at each iterations
+        A list with accumulated time for every iterations
 
     Example
     -------
     >>> import numpy as np
-    >>> import nmf
+    >>> from nn_fac import nmf
     >>> rank = 5
     >>> U_lines = 100
     >>> V_col = 125
@@ -120,7 +119,7 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
     >>> M = U_0@V_0
     >>> U, V = nmf.nmf(M, rank, init = "random", n_iter_max = 500, tol = 1e-8,
                sparsity_coefficients = [None, None], fixed_modes = [], normalize = [False, False],
-               verbose=True, return_errors = False)
+               verbose=True, return_costs = False)
 
     References
     ----------
@@ -136,7 +135,6 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
     factorization", Journal of Machine Learning Research 13.Mar (2012),
     pp. 849{853.
     """
-
     if init.lower() == "random":
         k, n = data.shape
         U_0 = np.random.rand(k, rank)
@@ -154,15 +152,12 @@ def nmf(data, rank, init = "random", U_0 = None, V_0 = None, n_iter_max=100, tol
 
     return compute_nmf(data, rank, U_0, V_0, n_iter_max=n_iter_max, tol=tol,
                    sparsity_coefficients = sparsity_coefficients, fixed_modes = fixed_modes, normalize = normalize,
-                   verbose=verbose, return_errors=return_errors)
-
-
-
+                   verbose=verbose, return_costs=return_costs)
 
 # Author : Jeremy Cohen, modified by Axel Marmoret
 def compute_nmf(data, rank, U_in, V_in, n_iter_max=100, tol=1e-8,
            sparsity_coefficients = [None, None], fixed_modes = [], normalize = [False, False],
-           verbose=False, return_errors=False):
+           verbose=False, return_costs=False):
     """
     Computation of a Nonnegative matrix factorization via
     hierarchical alternating least squares (HALS) [1],
@@ -182,9 +177,9 @@ def compute_nmf(data, rank, U_in, V_in, n_iter_max=100, tol=1e-8,
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficients: List of float (two)
         The sparsity coefficients on U and V respectively.
@@ -194,25 +189,25 @@ def compute_nmf(data, rank, U_in, V_in, n_iter_max=100, tol=1e-8,
         Has to be set not to update a factor, 0 and 1 for U and V respectively
         Default: []
     normalize: List of boolean (two)
-        A boolean whereas the factors need to be normalized.
+        Indicates whether the factors need to be normalized or not.
         The normalization is a l_2 normalization on each of the rank components
         (columnwise for U, linewise for V)
         Default: [False, False]
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
     -------
     U, V: numpy arrays
         Factors of the NMF
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list
         A list with accumulated time at each iterations
 
@@ -222,11 +217,10 @@ def compute_nmf(data, rank, U_in, V_in, n_iter_max=100, tol=1e-8,
     Hierarchical ALS Algorithms for Nonnegative Matrix Factorization,
     Neural Computation 24 (4): 1085-1105, 2012.
     """
-
     # initialisation
     U = U_in.copy()
     V = V_in.copy()
-    rec_errors = []
+    cost_fct_vals = []
     norm_data = np.linalg.norm(data)
     tic = time.time()
     toc = []
@@ -241,33 +235,33 @@ def compute_nmf(data, rank, U_in, V_in, n_iter_max=100, tol=1e-8,
     for iteration in range(n_iter_max):
 
         # One pass of least squares on each updated mode
-        U, V, rec_error = one_nmf_step(data, rank, U, V, norm_data,
+        U, V, cost = one_nmf_step(data, rank, U, V, norm_data,
                                        sparsity_coefficients, fixed_modes, normalize)
 
         toc.append(time.time() - tic)
-        if tol:
-            rec_errors.append(rec_error)
 
-            if verbose:
-                if iteration == 0:
-                    print('reconstruction error={}'.format(rec_errors[iteration]))
+        cost_fct_vals.append(cost)
+
+        if verbose:
+            if iteration == 0:
+                print('Normalized cost function value={}'.format(cost))
+            else:
+                if cost_fct_vals[-2] - cost_fct_vals[-1] > 0:
+                    print('Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]))
                 else:
-                    if rec_errors[-2] - rec_errors[-1] > 0:
-                        print('reconstruction error={}, variation={}.'.format(
-                                rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
-                    else:
-                        # print in red when the reconstruction error is negative (shouldn't happen)
-                        print('\033[91m' + 'reconstruction error={}, variation={}.'.format(
-                                rec_errors[-1], rec_errors[-2] - rec_errors[-1]) + '\033[0m')
+                    # print in red when the reconstruction error is negative (shouldn't happen)
+                    print('\033[91m' + 'Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]) + '\033[0m')
 
-            if iteration > 0 and abs(rec_errors[-2] - rec_errors[-1]) < tol:
-                # Stop condition: relative error between last two iterations < tol
-                if verbose:
-                    print('converged in {} iterations.'.format(iteration))
-                break
+        if iteration > 0 and abs(cost_fct_vals[-2] - cost_fct_vals[-1]) < tol:
+            # Stop condition: relative error between last two iterations < tol
+            if verbose:
+                print('Converged in {} iterations.'.format(iteration))
+            break
 
-    if return_errors:
-        return np.array(U), np.array(V), rec_errors, toc
+    if return_costs:
+        return np.array(U), np.array(V), cost_fct_vals, toc
     else:
         return np.array(U), np.array(V)
 
@@ -307,8 +301,9 @@ def one_nmf_step(data, rank, U_in, V_in, norm_data,
     -------
     U, V: numpy arrays
         Factors of the NMF
-    rec_error:
-        The reconstruction error of this NMF step
+    cost_fct_val:
+        The value of the cost function at this step,
+        normalized by the squared norm of the original matrix.
     """
 
     if len(sparsity_coefficients) != 2:
@@ -354,8 +349,8 @@ def one_nmf_step(data, rank, U_in, V_in, norm_data,
 
     sparsity_coefficients = np.where(np.array(sparsity_coefficients) == None, 0, sparsity_coefficients)
 
-    rec_error = np.linalg.norm(data-np.dot(U,V), ord='fro') ** 2 + 2 * (sparsity_coefficients[0] * np.linalg.norm(U, ord=1) + sparsity_coefficients[1] * np.linalg.norm(V, ord=1))
+    cost = np.linalg.norm(data-np.dot(U,V), ord='fro') ** 2 + 2 * (sparsity_coefficients[0] * np.linalg.norm(U, ord=1) + sparsity_coefficients[1] * np.linalg.norm(V, ord=1))
 
-    rec_error = rec_error/norm_data
+    cost = cost/(norm_data**2)
 
-    return U, V, rec_error
+    return U, V, cost

nn_fac/ntd.py
@@ -16,7 +16,7 @@ import nn_fac.errors as err
 
 def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_max=100, tol=1e-6,
            sparsity_coefficients = [], fixed_modes = [], normalize = [], mode_core_norm = None, hals = False,
-           verbose=False, return_errors=False, deterministic=False):
+           verbose=False, return_costs=False, deterministic=False):
 
     """
     ======================================
@@ -81,10 +81,10 @@ def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_ma
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
-        Default: 1e-8
+        Threshold on the improvement in cost function value.
+        Between two succesive iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
+        Default: 1e-6
     sparsity_coefficients: list of float (as much as the number of modes + 1 for the core)
         The sparsity coefficients on each factor and on the core respectively.
         If set to None or [], the algorithm is computed without sparsity
@@ -93,7 +93,7 @@ def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_ma
         Has to be set not to update a factor, taken in the order of modes and lastly on the core.
         Default: []
     normalize: list of boolean (as much as the number of modes + 1 for the core)
-        A boolean whereas the factors need to be normalized.
+        Indicates whether the factors need to be normalized or not.
         The normalization is a l_2 normalization on each of the rank components
         (For the factors, each column will be normalized, ie each atom of the dimension of the current rank).
         Default: []
@@ -106,11 +106,12 @@ def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_ma
         Whether to run hals (true) or gradient (false) update on the core.
         Default (and recommanded): false
     verbose: boolean
-        Indicates whether the algorithm prints the successive reconstruction errors or not.
+        Indicates whether the algorithm prints the successive
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not.
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
     deterministic:
         Runs the algorithm as a deterministic way, by fixing seed in all possible randomisation,
@@ -123,8 +124,8 @@ def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_ma
         The core tensor linking the factors of the decomposition
     factors: numpy #TODO: For tensorly pulling, replace numpy by backend
         An array containing all the factors computed with the NTD
-    errors: list, only if return_errors == True
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list, only if return_errors == True
         A list with accumulated time at each iterations
 
@@ -219,11 +220,11 @@ def ntd(tensor, ranks, init = "random", core_0 = None, factors_0 = [], n_iter_ma
     return compute_ntd(tensor, ranks, core, factors, n_iter_max=n_iter_max, tol=tol,
                        sparsity_coefficients = sparsity_coefficients, fixed_modes = fixed_modes, 
                        normalize = normalize, mode_core_norm = mode_core_norm,
-                       verbose=verbose, return_errors=return_errors, hals = hals, deterministic = deterministic)
+                       verbose=verbose, return_costs=return_costs, hals = hals, deterministic = deterministic)
 
 def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
            sparsity_coefficients = [], fixed_modes = [], normalize = [], hals = False,mode_core_norm=None,
-           verbose=False, return_errors=False, deterministic=False):
+           verbose=False, return_costs=False, deterministic=False):
 
     """
     Computation of a Nonnegative Tucker Decomposition [1]
@@ -248,9 +249,9 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficients: list of float (as much as the number of modes + 1 for the core)
         The sparsity coefficients on each factor and on the core respectively.
@@ -260,7 +261,7 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
         Has to be set not to update a factor, taken in the order of modes and lastly on the core.
         Default: []
     normalize: list of boolean (as much as the number of modes + 1 for the core)
-        A boolean whereas the factors need to be normalized.
+        Indicates whether the factors need to be normalized or not.
         The normalization is a l_2 normalization on each of the rank components
         (For the factors, each column will be normalized, ie each atom of the dimension of the current rank).
         Default: []
@@ -273,11 +274,12 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
         Whether to run hals (true) or gradient (false) update on the core.
         Default (and recommanded): false
     verbose: boolean
-        Indicates whether the algorithm prints the successive reconstruction errors or not.
+        Indicates whether the algorithm prints the successive
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not.
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
     deterministic:
         Runs the algorithm as a deterministic way, by fixing seed in all possible randomisation.
@@ -289,8 +291,8 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
         The core tensor linking the factors of the decomposition
     factors: numpy #TODO: For tensorly pulling, replace numpy by backend
         An array containing all the factors computed with the NTD
-    errors: list, only if return_errors == True
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list, only if return_errors == True
         A list with accumulated time at each iterations
 
@@ -335,7 +337,7 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
         print("The core was asked NOT to be normalized, but mode_core_norm was set to a valid norm. Is this a mistake?")
     
     # initialisation - declare local varaibles
-    rec_errors = []
+    cost_fct_vals = []
     tic = time.time()
     toc = []
 
@@ -348,37 +350,37 @@ def compute_ntd(tensor_in, ranks, core_in, factors_in, n_iter_max=100, tol=1e-6,
     for iteration in range(n_iter_max):
         # One pass of least squares on each updated mode
         if deterministic:
-            core, factors, rec_error = one_ntd_step(tensor, ranks, core, factors, norm_tensor,
+            core, factors, cost = one_ntd_step(tensor, ranks, core, factors, norm_tensor,
                                           sparsity_coefficients, fixed_modes, normalize, mode_core_norm, hals = hals, alpha = math.inf)
         else:
-            core, factors, rec_error = one_ntd_step(tensor, ranks, core, factors, norm_tensor,
+            core, factors, cost = one_ntd_step(tensor, ranks, core, factors, norm_tensor,
                                           sparsity_coefficients, fixed_modes, normalize, mode_core_norm, hals = hals)
 
         # Store the computation time
         toc.append(time.time() - tic)
 
-        rec_errors.append(rec_error)
+        cost_fct_vals.append(cost)
 
         if verbose:
             if iteration == 0:
-                print('reconstruction error={}'.format(rec_errors[iteration]))
+                print('Normalized cost function value={}'.format(cost))
             else:
-                if rec_errors[-2] - rec_errors[-1] > 0:
-                    print('reconstruction error={}, variation={}.'.format(
-                            rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+                if cost_fct_vals[-2] - cost_fct_vals[-1] > 0:
+                    print('Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]))
                 else:
-                    print('\033[91m' + 'reconstruction error={}, variation={}.'.format(
-                            rec_errors[-1], rec_errors[-2] - rec_errors[-1]) + '\033[0m')
+                    # print in red when the reconstruction error is negative (shouldn't happen)
+                    print('\033[91m' + 'Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]) + '\033[0m')
 
-        if iteration > 0 and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+        if iteration > 0 and abs(cost_fct_vals[-2] - cost_fct_vals[-1]) < tol:
             # Stop condition: relative error between last two iterations < tol
             if verbose:
-                print('converged in {} iterations.'.format(iteration))
+                print('Converged in {} iterations.'.format(iteration))
             break
 
-
-    if return_errors:
-        return core, factors, rec_errors, toc
+    if return_costs:
+        return core, factors, cost_fct_vals, toc
     else:
         return core, factors
 
@@ -448,9 +450,10 @@ def one_ntd_step(tensor, ranks, in_core, in_factors, norm_tensor,
         The core tensor linking the factors of the decomposition
     factors: list of factors
         An array containing all the factors computed with the NTD
-    error: float
-        The reconstruction error after this update.
-
+    cost_fct_val:
+        The value of the cost function at this step,
+        normalized by the squared norm of the original tensor.
+    
     References
     ----------
     [1] Tamara G Kolda and Brett W Bader. "Tensor decompositions and applications",
@@ -595,9 +598,9 @@ def one_ntd_step(tensor, ranks, in_core, in_factors, norm_tensor,
                 raise NotImplementedError("TODEBUG: Too many sparsity coefficients, should have been raised before.")
 
     rec_error = norm_tensor ** 2 - 2*tl.tenalg.inner(all_MtX, core) + tl.tenalg.inner(tl.tenalg.multi_mode_dot(core, all_MtM, transpose = False), core)
-    rec_error = (rec_error ** (1/2) + sparsity_error) / norm_tensor
+    cost_fct_val = (rec_error + sparsity_error) / (norm_tensor ** 2)
 
     #exhaustive_rec_error = (tl.norm(tensor - tl.tenalg.multi_mode_dot(core, factors, transpose = False), 2) + sparsity_error) / norm_tensor
     #print("diff: " + str(rec_error - exhaustive_rec_error))
     #print("max" + str(np.amax(factors[2])))
-    return core, factors, rec_error #  exhaustive_rec_error
+    return core, factors, cost_fct_val #  exhaustive_rec_error

nn_fac/ntf.py
@@ -13,7 +13,7 @@ from nimfa.methods import seeding
 
 def ntf(tensor, rank, init = "random", factors_0 = [], n_iter_max=100, tol=1e-8,
            sparsity_coefficients = [], fixed_modes = [], normalize = [],
-           verbose=False, return_errors=False):
+           verbose=False, return_costs=False):
 
     """
     ======================================
@@ -74,9 +74,9 @@ def ntf(tensor, rank, init = "random", factors_0 = [], n_iter_max=100, tol=1e-8,
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two succesive iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficients: array of float (as much as the number of modes)
         The sparsity coefficients on U and V respectively.
@@ -92,19 +92,19 @@ def ntf(tensor, rank, init = "random", factors_0 = [], n_iter_max=100, tol=1e-8,
         Default: []
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
     -------
     np.array(factors): numpy array
         An array containing all the factors computed with PARAFAC decomposition
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list
         A list with accumulated time at each iterations
 
@@ -123,7 +123,7 @@ def ntf(tensor, rank, init = "random", factors_0 = [], n_iter_max=100, tol=1e-8,
     >>> T = ntf.tl.kruskal_tensor.kruskal_to_tensor(factors_0)
     >>> factors = ntf.ntf(T, rank, init = "random", n_iter_max = 500, tol = 1e-8,
                sparsity_coefficients = [None, None, None], fixed_modes = [], normalize = [False, False, False],
-               verbose = True, return_errors = False)
+               verbose = True, return_costs = False)
 
     References
     ----------
@@ -178,11 +178,11 @@ def ntf(tensor, rank, init = "random", factors_0 = [], n_iter_max=100, tol=1e-8,
 
     return compute_ntf(tensor, rank, factors, n_iter_max=n_iter_max, tol=tol,
                        sparsity_coefficients = sparsity_coefficients, fixed_modes = fixed_modes, normalize = normalize,
-                       verbose=verbose, return_errors=return_errors)
+                       verbose=verbose, return_costs=return_costs)
 
 def compute_ntf(tensor_in, rank, factors_in, n_iter_max=100, tol=1e-8,
            sparsity_coefficients = [], fixed_modes = [], normalize = [],
-           verbose=False, return_errors=False):
+           verbose=False, return_costs=False):
 
     """
     Computation of a Nonnegative matrix factorization via
@@ -201,9 +201,9 @@ def compute_ntf(tensor_in, rank, factors_in, n_iter_max=100, tol=1e-8,
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficients: List of float (as much as the number of modes)
         The sparsity coefficients on U and V respectively.
@@ -219,19 +219,19 @@ def compute_ntf(tensor_in, rank, factors_in, n_iter_max=100, tol=1e-8,
         Default: []
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
     -------
     np.array(factors): numpy array
         An array containing all the factors computed with PARAFAC decomposition
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list
         A list with accumulated time at each iterations
 
@@ -265,7 +265,7 @@ def compute_ntf(tensor_in, rank, factors_in, n_iter_max=100, tol=1e-8,
         normalize = [False for i in range(nb_modes)]
 
     # initialisation - declare local varaibles
-    rec_errors = []
+    cost_fct_vals = []
     tic = time.time()
     toc = []
 
@@ -277,33 +277,33 @@ def compute_ntf(tensor_in, rank, factors_in, n_iter_max=100, tol=1e-8,
     # Iterate over one step of NTF
     for iteration in range(n_iter_max):
         # One pass of least squares on each updated mode
-        factors, rec_error = one_ntf_step(unfolded_tensors, rank, factors, norm_tensor,
+        factors, cost = one_ntf_step(unfolded_tensors, rank, factors, norm_tensor,
                                           sparsity_coefficients, fixed_modes, normalize)
         # Store the computation time
         toc.append(time.time() - tic)
 
-        rec_errors.append(rec_error)
+        cost_fct_vals.append(cost)
 
         if verbose:
             if iteration == 0:
-                print('reconstruction error={}'.format(rec_errors[iteration]))
+                print('Normalized cost function value={}'.format(cost))
             else:
-                if rec_errors[-2] - rec_errors[-1] > 0:
-                    print('reconstruction error={}, variation={}.'.format(
-                            rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+                if cost_fct_vals[-2] - cost_fct_vals[-1] > 0:
+                    print('Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]))
                 else:
-                    print('\033[91m' + 'reconstruction error={}, variation={}.'.format(
-                            rec_errors[-1], rec_errors[-2] - rec_errors[-1]) + '\033[0m')
+                    # print in red when the reconstruction error is negative (shouldn't happen)
+                    print('\033[91m' + 'Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]) + '\033[0m')
 
-        if iteration > 0 and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+        if iteration > 0 and abs(cost_fct_vals[-2] - cost_fct_vals[-1]) < tol:
             # Stop condition: relative error between last two iterations < tol
             if verbose:
-                print('converged in {} iterations.'.format(iteration))
+                print('Converged in {} iterations.'.format(iteration))
             break
 
-
-    if return_errors:
-        return np.array(factors), rec_errors, toc
+    if return_costs:
+        return np.array(factors), cost_fct_vals, toc
     else:
         return np.array(factors)
 
@@ -353,16 +353,14 @@ def one_ntf_step(unfolded_tensors, rank, in_factors, norm_tensor,
         of a PARAFAC computation.
         Default: 0.01
 
-
     Returns
     -------
     np.array(factors): numpy array
         An array containing all the factors computed with PARAFAC decomposition
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
-    toc: list
-        A list with accumulated time at each iterations
-
+    cost_fct_val:
+        The value of the cost function at this step,
+        normalized by the squared norm of the original tensor.
+        
     References
     ----------
     [1] Tamara G Kolda and Brett W Bader. "Tensor decompositions and applications",
@@ -411,6 +409,6 @@ def one_ntf_step(unfolded_tensors, rank, in_factors, norm_tensor,
 
     # error computation (improved using precomputed quantities)
     rec_error = norm_tensor ** 2 - 2*tl.dot(tl.tensor_to_vec(factors[mode]),tl.tensor_to_vec(rhs)) +  tl.norm(tl.dot(factors[mode],tl.transpose(krao)),2)**2
-    rec_error = (rec_error ** (1/2) + sparsity_error) / norm_tensor
+    cost_fct_val = (rec_error + sparsity_error) / (norm_tensor ** 2)
 
-    return factors, rec_error
+    return factors, cost_fct_val

nn_fac/parafac2.py
@@ -14,7 +14,7 @@ from nimfa.methods import seeding
 def parafac_2(tensor_slices, rank, init_with_P, init = "random", W_list_in = None, H = None, D_list_in = None, W_star = None, P_list = None,
     tol_mu = 1e6, step_mu = 1.02, n_iter_max=100, tol=1e-6,
     sparsity_coefficient = None, fixed_modes = [], normalize = [False, False, False, False, False],
-    verbose=False, return_errors=False):
+    verbose=False, return_costs=False):
 
     """
     ========
@@ -104,10 +104,10 @@ def parafac_2(tensor_slices, rank, init_with_P, init = "random", W_list_in = Non
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
-        Default: 1e-8
+        Threshold on the improvement in cost function value.
+        Between two succesive iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
+        Default: 1e-6
     sparsity_coefficient: float
         The sparsity coefficient on H.
         If set to None, the algorithm is computed without sparsity
@@ -116,16 +116,16 @@ def parafac_2(tensor_slices, rank, init_with_P, init = "random", W_list_in = Non
         Has to be set not to update a factor, 0 and 1 for U and V respectively
         Default: []
     normalize: array of boolean (5)
-        A boolean where the factors need to be normalized.
+        Indicates whether the factors need to be normalized or not.
         The normalization is a l_2 normalization on each of the rank components
         Default: [False, False, False, False, False]
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
@@ -149,7 +149,7 @@ def parafac_2(tensor_slices, rank, init_with_P, init = "random", W_list_in = Non
     >>> rank = 30
     >>> W_list, H, D_list = parafac2.parafac_2(T, rank, init_with_P = True, init = "nndsvd",
                                                sparsity_coefficient = None, fixed_modes = [], normalize = [True, False, True, True, True],
-                                               verbose=False)
+                                               verbose=True)
 
     References
     ----------
@@ -220,14 +220,14 @@ def parafac_2(tensor_slices, rank, init_with_P, init = "random", W_list_in = Non
 
     return compute_parafac_2(tensor_slices, rank, W_list_in = W_list, H_0 = H, D_list_in = D_list, init_with_P = init_with_P, W_star_in = W_star, P_list_in = P_list, n_iter_max=n_iter_max, tol=tol,
               sparsity_coefficient = sparsity_coefficient, fixed_modes = fixed_modes, normalize = [False, False, False, False],
-              verbose=verbose, return_errors=return_errors)
+              verbose=verbose, return_costs=return_costs)
 
 
 # Author : Jeremy Cohen, modified by Axel Marmoret
 def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_P, W_star_in = None, P_list_in = None,
     tol_mu = 1e6, step_mu = 1.02, n_iter_max=100, tol=1e-8,
     sparsity_coefficient = None, fixed_modes = [], normalize = [False, False, False, False, False],
-    verbose=False, return_errors=False):
+    verbose=False, return_costs=False):
 
     """
     Factorization of a third-order tensor T in nonnegative matrices,
@@ -276,9 +276,9 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
         The maximal number of iteration before stopping the algorithm
         Default: 100
     tol: float
-        Threshold on the improvement in reconstruction error.
-        Between two iterations, if the reconstruction error difference is
-        below this threshold, the algorithm stops.
+        Threshold on the improvement in cost function value.
+        Between two iterations, if the difference between 
+        both cost function values is below this threshold, the algorithm stops.
         Default: 1e-8
     sparsity_coefficient: float
         The sparsity coefficient on H.
@@ -293,11 +293,11 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
         Default: [False, False, False, False, False]
     verbose: boolean
         Indicates whether the algorithm prints the successive
-        reconstruction errors or not
+        normalized cost function values or not
         Default: False
-    return_errors: boolean
-        Indicates whether the algorithm should return all reconstruction errors
-        and computation time of each iteration or not
+    return_costs: boolean
+        Indicates whether the algorithm should return all normalized cost function 
+        values and computation time of each iteration or not
         Default: False
 
     Returns
@@ -308,8 +308,8 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
         Updated factor H
     D_list:
         List of the updated D_k
-    errors: list
-        A list of reconstruction errors at each iteration of the algorithm.
+    cost_fct_vals: list
+        A list of the normalized cost function values, for every iteration of the algorithm.
     toc: list
         A list with accumulated time at each iterations
 
@@ -344,7 +344,7 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
         P_list = P_list_in.copy()
 
     # initialization - declare local varaibles
-    rec_errors = []
+    cost_fct_vals = []
     tic = time.time()
     toc = []
     norm_slices = []
@@ -364,9 +364,9 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
     for iteration in range(n_iter_max):
 
         if iteration == 0:
-            previous_rec_error = None
+            previous_cost_fct_val = None
         else:
-            previous_rec_error = rec_errors[-1]
+            previous_cost_fct_val = cost_fct_vals[-1]
 
         # Increase mu
         if iteration == 1:
@@ -376,57 +376,53 @@ def compute_parafac_2(tensor_slices, rank, W_list_in, H_0, D_list_in, init_with_
         if iteration == 2:
             increasing_mu = True
 
-        W_list, H, D_list, W_star, P_list, mu_list, rec_error, couple_error, increasing_mu = one_step_parafac2(tensor_slices, rank, W_list, H, D_list, mu_list, norm_slices,
-                                                                                                       previous_rec_error, increasing_mu = increasing_mu,
+        W_list, H, D_list, W_star, P_list, mu_list, cost, couple_error, increasing_mu = one_step_parafac2(tensor_slices, rank, W_list, H, D_list, mu_list, norm_slices,
+                                                                                                       previous_cost_fct_val, increasing_mu = increasing_mu,
                                                                                                        init_with_P = init_with_P, P_list_in = P_list, W_star_in = W_star,
                                                                                                        sparsity_coefficient = sparsity_coefficient, fixed_modes = fixed_modes, normalize = normalize)
 
         # Store the computation time
         toc.append(time.time() - tic)
 
-        rec_errors.append(rec_error)
+        cost_fct_vals.append(cost)
         couple_errors.append(couple_error)
 
         if verbose:
             if iteration == 0:
-                print('reconstruction error={}'.format(rec_errors[iteration]))
+                print('Normalized cost function value={}'.format(cost))
+            
                 for k in range(nb_channel):
-                    print('couple_error for channel {} = {}'.format(k, (couple_errors[iteration])[k]))
+                    print('Couple_error for channel {} = {}'.format(k, (couple_errors[iteration])[k]))
             else:
-                if rec_errors[-2] - rec_errors[-1] > 0:
-                    print('reconstruction error={}, variation={}.'.format(
-                            rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+                if cost_fct_vals[-2] - cost_fct_vals[-1] > 0:
+                    print('Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]))
                 else:
-                    if rec_errors[-2] - rec_errors[-1] > 0:
-                        print('reconstruction error={}, variation={}.'.format(
-                                rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
-                    else:
-                        print('\033[91m' + 'reconstruction error={}, variation={}.'.format(
-                                rec_errors[-1], rec_errors[-2] - rec_errors[-1]) + '\033[0m')
+                    # print in red when the reconstruction error is negative (shouldn't happen)
+                    print('\033[91m' + 'Normalized cost function value={}, variation={}.'.format(
+                            cost_fct_vals[-1], cost_fct_vals[-2] - cost_fct_vals[-1]) + '\033[0m')
 
                 for k in range(nb_channel):
                     if (couple_errors[-2])[k] - (couple_errors[-1])[k] > 0:
-                        print('couple_error for channel {} = {}, variation={}.'.format(k, (couple_errors[-1])[k],
+                        print('Couple_error for channel {} = {}, variation={}.'.format(k, (couple_errors[-1])[k],
                           (couple_errors[-2])[k] - (couple_errors[-1])[k]))
                     else:
-                        print('\033[91m' + 'couple_error for channel {} = {}, variation={}.'.format(k, (couple_errors[-1])[k],
+                        print('\033[91m' + 'Couple_error for channel {} = {}, variation={}.'.format(k, (couple_errors[-1])[k],
                           (couple_errors[-2])[k] - (couple_errors[-1])[k]) + '\033[0m')
 
-
-        if iteration > 0 and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+        if iteration > 0 and abs(cost_fct_vals[-2] - cost_fct_vals[-1]) < tol:
             # Stop condition: relative error between last two iterations < tol
             if verbose:
-                print('converged in {} iterations.'.format(iteration))
+                print('Converged in {} iterations.'.format(iteration))
             break
 
-
-    if return_errors:
-        return W_list, np.array(H), D_list, rec_errors, toc
+    if return_costs:
+        return W_list, np.array(H), D_list, cost_fct_vals, toc
     else:
         return W_list, np.array(H), D_list
 
 def one_step_parafac2(slices, rank, W_list_in, H_in, D_list_in, mu_list_in, norm_slices,
-                      previous_rec_error, increasing_mu = True, tol_mu = 1e6, step_mu = 1.02,
+                      previous_cost_fct_val, increasing_mu = True, tol_mu = 1e6, step_mu = 1.02,
                       init_with_P = True, W_star_in = None, P_list_in = None,
                       sparsity_coefficient = None, fixed_modes = [], normalize = [False, False, False, False, False]):
 
@@ -496,17 +492,16 @@ def one_step_parafac2(slices, rank, W_list_in, H_in, D_list_in, mu_list_in, norm
     -------
     W_list, D_list, H, W_star, mu_list:
         The updated factors
-    rec_error : float
-        residual error after the ALS steps.
-
-
+    cost_fct_val:
+        The value of the cost function at this step,
+        normalized by the squared norm of the original tensor.
     """
     W_list = W_list_in.copy()
     D_list = D_list_in.copy()
 
     H = H_in.copy()
     mu_list = mu_list_in.copy()
-    rec_error = 0
+    cost_fct_val = 0
     nb_channel = len(W_list)
 
     # Tous les append doivent etre reinit ici
@@ -612,21 +607,21 @@ def one_step_parafac2(slices, rank, W_list_in, H_in, D_list_in, mu_list_in, norm
     couple_error = []
 
     if sparsity_coefficient != None:
-        rec_error = sparsity_coefficient * np.linalg.norm(H, ord=1)
+        cost_fct_val = sparsity_coefficient * np.linalg.norm(H, ord=1)
 
     for k in range(nb_channel):
         couple_error.append(np.linalg.norm(W_list[k] - P_list[k]@W_star, ord='fro'))
 
         slice_rec_error = np.linalg.norm(slices[k]-W_list[k]@D_list[k]@H)** 2 + (mu_list[k] * couple_error[k]**2) / norm_slices[k]
-        rec_error += slice_rec_error
+        cost_fct_val += slice_rec_error
 
-        if previous_rec_error != None:
-            if mu_list[k] < tol_mu and (previous_rec_error - rec_error) > 0 and increasing_mu:
+        if previous_cost_fct_val != None:
+            if mu_list[k] < tol_mu and (previous_cost_fct_val - cost_fct_val) > 0 and increasing_mu:
                 mu_list[k] *= step_mu
             elif increasing_mu: # Stop increasing mu for the next iterations
                 increasing_mu = False
 
-    return W_list, H, D_list, W_star, P_list, mu_list, rec_error, couple_error, increasing_mu
+    return W_list, H, D_list, W_star, P_list, mu_list, cost_fct_val, couple_error, increasing_mu
 
 
 def compute_P_k(W_list, W_star, nb_channel):