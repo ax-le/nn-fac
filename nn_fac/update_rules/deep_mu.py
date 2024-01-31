418f71405012743cbe9ac6327b98474778359815
Axel Marmoret <axel.marmoret@imt-atlantique.fr>
2024-01-31 17:54:27 +0100

    Adding a small parameter to the divide in deep mu to avoid numerical instability.

--- nn_fac/update_rules/deep_mu.py
+++ nn_fac/update_rules/deep_mu.py
@@ -7,7 +7,8 @@ def deep_KL_mu(W_Lm1, W_L, H_L, WH_Lp1, lambda_):
     ONES = np.ones_like(W_Lm1)
     a = ONES @ H_L.T - lambda_ * np.log(WH_Lp1)
     b = W_L * ((W_Lm1 / (W_L @ H_L)) @ H_L.T)
-    W_L = np.maximum(eps, (1/lambda_ * b) / (lambertw(b * np.exp(a/lambda_) / lambda_, k=0).real))
+    lambert = lambertw(b * np.exp(a/lambda_) / lambda_, k=0).real
+    W_L = np.maximum(eps, (1/lambda_ * b) / (lambert + eps))
 
     return W_L
 
